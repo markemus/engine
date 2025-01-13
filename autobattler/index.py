57cb176f02e32772893fcf730d13049ecee5e5d2
markemus <markemusgames@gmail.com>
2025-01-13 08:31:06 +0200

    updated todos

--- autobattler/index.py
+++ autobattler/index.py
@@ -1,4 +1,42 @@
 # TODO golem fights (owners are present but neutral)
 # TODO shop
 # TODO crowd effect (more cheering = more gold)
+#  more cash for a close fight (so you can rebuild your golem)
+#  extra cash for every enemy limb removed
+#  more cash for longer fight
 # TODO classes- affect items in shop
+#  torturer- targets small limbs and extends fight
+#  tank- draws aggro, uses heavy armor
+#  mage- uses adds, reduced aggro, no armor
+#  rogue- small limbs, light armor
+# TODO different combat ais per class (limb targeting strategies and weapon choice)
+# TODO synergies.
+#  Suck blood from entangled limbs.
+#  Heal from fire (spread it everywhere).
+#  Fire shield (takes damage and breaks, but heals from fire).
+#  Oil slick that trips enemies that can be lit on fire (anything with oil takes dot).
+#  Acid- like fire, but different synergies.
+#  Buff damage from damage type
+#  Slime- coat a limb and reduce damage from it. Puts out fire.
+#  Digestive slime- heal yourself for every enemy limb coated in slime. Does a little damage.
+#  Revive cut off limbs to fight for you (and buzzsaw).
+#  Pulp limbs so they can't revive.
+#  Buff your little allies.
+#  Energy shield that covers allies as well.
+#  Orbital- orb that blocks
+#  Illusions- allies that don't do damage but draw aggro (somewhat countered by pest ai)
+#  Allies that draw aggro and tank an attack or two.
+#  AOE attacks (multiple enemies), counters adds.
+#  Hook (pull enemies out of the sky)
+#  Ranged weapons countered by armor. (use ammo?)
+#  Melee weapons countered by blockers.
+#  Reactive armor- light weapon on fire, spray it with acid
+#  Armor breach- reduce limb armor
+#  Weaken enemy limb to reduce damage. Allow this to stack so you can build a debuffing build that grows over time
+#  Double tap- next attack happens twice.
+#  Effects that only occur after N rounds.
+#  Dispel- remove N of creature's effects. Counters effect builds.
+#  A weapon/effect that buffs, heals, damages based on how many of same damage type weapons you have
+#  Limb that draws attacks to it
+#  Consumable limbs/equipment that leave permanent buffs behind
+#  Static- electricity zap on attack. Messes with mechanical and doesn't affect magical.
