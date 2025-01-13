ad58e4d8de09b6016d3d7b89da6373264edb98de
markemus <markemusgames@gmail.com>
2025-01-13 08:47:25 +0200

    fixed bug with saver where it couldn't save large games

--- engine/save.py
+++ engine/save.py
@@ -1,9 +1,11 @@
 """Save and restore games."""
+import sys
 import dill
 
 
 def save(interface, savepath):
     """Pass an engine.interface.Interface() object in to be saved. This should save the entire game hierarchy."""
+    sys.setrecursionlimit(10000)
     dill.dump(interface, open(savepath, "wb"))
 
 def load(savepath):
